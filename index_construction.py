a1572c35780e8a65a762bd0d9b5f2ff4b674c026
zdelima <zdelima@uci.edu>
2025-06-03 23:55:00 -0700

    finalized m3 with merge-based index construction, tf idf scoring, and report

--- index_construction.py
+++ index_construction.py
@@ -2,6 +2,9 @@ import os
 import re
 import json
 import sys
+import math
+import heapq
+
 from bs4 import BeautifulSoup
 from nltk.stem import PorterStemmer
 from collections import defaultdict, Counter
@@ -18,8 +21,13 @@ def build_index(corpus_dir: str) -> tuple[dict, int]:
     index = defaultdict(list)
     term_ids = {}
     doc_ids = {}
+    df_counter = Counter()
+    
     term_id = 0
     doc_id = 0
+    file_count = 0
+    partial_count = 0
+    partial_files = []
     
     for root, _, files in os.walk(corpus_dir):
         for fname in sorted(files):
@@ -56,7 +64,33 @@ def build_index(corpus_dir: str) -> tuple[dict, int]:
                 tid = term_ids[term]
                 index[tid].append([doc_ids[url], freq])
                 
-    return term_ids, doc_ids, index
+            unique_tokens = set(tokens)
+            
+            for term in unique_tokens:
+                tid = term_ids[term]
+                df_counter[tid] += 1
+                
+            file_count += 1
+            
+            if file_count % 1000 == 0:
+                print(f"Saving to partial_index_{partial_count}")
+                tfidf_index = convert_tf_to_tfidf(index, df_counter, doc_id)
+                name = f"partial_index_{partial_count}.txt"
+                save_index(tfidf_index, name)
+                partial_files.append(name)
+                
+                index.clear()
+                df_counter.clear()
+                partial_count += 1
+
+    if index:
+        print(f"Saving to partial_index_{partial_count}")
+        tfidf_index = convert_tf_to_tfidf(index, df_counter, doc_id)
+        name = f"partial_index_{partial_count}.txt"
+        save_index(tfidf_index, name)
+        partial_files.append(name)
+
+    return term_ids, doc_ids, partial_files
 
 def save_index(obj, filename):
     mmap = {}
@@ -64,15 +98,34 @@ def save_index(obj, filename):
     with open(filename, 'w', encoding='utf-8') as f:
         for term_id in sorted(obj.keys()):
             mmap[term_id] = f.tell()
-            postings = " ".join(f"{doc_id},{tf}" for doc_id, tf in index[term_id])
+            postings = " ".join(f"{doc_id},{tf}" for doc_id, tf in obj[term_id])
             f.write(f"{term_id} : {postings}\n")
-            
-    with open("mmap.json", "w") as f2:
-        json.dump(mmap, f2)
 
-def save_json(obj, filename):
+def convert_tf_to_tfidf(index, df_counter, total_docs):
+    tfidf_index = {}
+    for tid, postings in index.items():
+        df = df_counter[tid]
+
+        idf = math.log(total_docs / df)
+        
+        tfidf_index[tid] = []
+        for doc_id, tf in postings:
+            score = (1 + math.log(tf)) * idf
+            score = round(score, 5)
+            tfidf_index[tid].append([doc_id, score])
+            
+    return tfidf_index
+            
+def save_terms(term_ids, filename):
     with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(obj, f)
+        for term, tid in term_ids.items():
+            f.write(f"{term}\t{tid}\n")
+            
+def save_docs(doc_ids, filename):
+    inverted = invert_map(doc_ids)
+    with open(filename, 'w', encoding='utf-8') as f:
+        for doc_id, url in inverted.items():
+            f.write(f"{doc_id}\t{url}\n")
 
 def compute_index_size(filepath):
     return os.path.getsize(filepath) / 1024
@@ -94,15 +147,68 @@ def invert_map(in_map):
         inverted_map[k] = j
     return inverted_map
 
+def merge_partial_indexes(partial_files, final_index="index.txt", mmap_file="mmap.txt"):
+    file_handles = [open(fname, "r", encoding="utf-8") for fname in partial_files] # open every partial file
+    heap = []
+
+    for i, f in enumerate(file_handles): # for every opened file
+        line = f.readline() # read the first line
+        if line: 
+            tid = int(line.split(":", 1)[0].strip()) # given that this is a prtia lposting, the first value when split will be the term id
+            heapq.heappush(heap, (tid, line, i)) # push that line into the heap
+
+    with open(final_index, "w", encoding="utf-8") as index_out, open(mmap_file, "w", encoding="utf-8") as mmap_out:
+        current_tid = None
+        current_postings = []
+
+        while heap:
+            tid, line, file_idx = heapq.heappop(heap) # get the first value in the heap, based on the term id
+            _, postings_str = line.strip().split(":", 1) # get the string of postings from that tid
+            postings = [[int(doc), float(score)] for doc, score in (p.split(",") for p in postings_str.strip().split())] # make int docid to score pair 
+
+            if current_tid is None:
+                current_tid = tid
+                current_postings = postings
+            elif tid == current_tid:
+                current_postings.extend(postings) # add to list
+            else:
+                offset = index_out.tell() # get bit position in index tid is at
+                current_postings.sort(key=lambda x: x[0]) # sort by doc id
+                merged_str = " ".join(f"{int(doc_id)},{round(score, 5)}" for doc_id, score in current_postings) # convert to correct string format
+                index_out.write(f"{current_tid} : {merged_str}\n") # write merged posting to index
+                mmap_out.write(f"{current_tid}\t{offset}\n") # write term id : offset pair to mmap for seeking
+
+                current_tid = tid # move to next tid
+                current_postings = postings
+
+            next_line = file_handles[file_idx].readline() # read next line and push to heap
+            if next_line:
+                next_tid = int(next_line.split(":", 1)[0].strip())
+                heapq.heappush(heap, (next_tid, next_line, file_idx))
+
+        if current_tid is not None: # merge the final tid
+            offset = index_out.tell()
+            merged_str = " ".join(f"{int(doc_id)},{round(score, 5)}" for doc_id, score in current_postings)
+            index_out.write(f"{current_tid} : {merged_str}\n")
+            mmap_out.write(f"{current_tid}\t{offset}\n")
+
+    for f in file_handles: # close all files
+        f.close()
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python indexer.py <corpus_dir>")
         sys.exit(1)
     data_dir   = sys.argv[1]
     
-    term_ids, doc_ids, index = build_index(data_dir)
+    term_ids, doc_ids, partial_files = build_index(data_dir)
     
-    save_json(term_ids, "terms.json")
-    save_json(invert_map(doc_ids), "docs.json")
-    save_index(index, "index.txt")
+    merge_partial_indexes(partial_files, final_index="index.txt", mmap_file="mmap.txt")
+    
+    save_terms(term_ids, "terms.txt")
+    save_docs(doc_ids, "docs.txt")
+    
+    for f in partial_files:
+        os.remove(f)
 
--- search_engine.py
+++ search_engine.py
@@ -1,8 +1,7 @@
-import json
 import re
 import time
 import mmap
-# import psutil # used to check memory usage
+import psutil # used to check memory usage
 import os
 from nltk.stem import PorterStemmer
 
@@ -15,9 +14,8 @@ def get_term_id(input, terms_file):
     porter_stemmer = PorterStemmer()
     stemmed_tokens = [porter_stemmer.stem(token) for token in filtered]
     
-        
     for token in stemmed_tokens:
-        tid = (terms_file.get(token))
+        tid = terms_file.get(token)
         if tid is not None:
             out.add(tid)
         
@@ -30,19 +28,21 @@ def get_doc_id_from_term_ids(term_ids, toc, mm):
         if tid is None:
             continue
         
-    offset = toc[str(tid)]
-    mm.seek(offset)
-    line = mm.readline().decode("utf-8").strip()
+        offset = toc[tid]
+        mm.seek(offset)
+        line = mm.readline().decode("utf-8").strip()
 
-    _, postings_str = line.split(":", 1)
-    postings = [list(map(int, pair.split(","))) for pair in postings_str.strip().split()]
-    out.append(postings)
+        _, postings_str = line.split(":", 1)
+        postings = [list(map(float, pair.split(","))) for pair in postings_str.strip().split()]
+        out.append(postings)
         
     return out
           
 def binary_and(inputs):
     if len(inputs) == 1:
         return inputs[0]
+    
+    inputs.sort(key=len) 
 
     result = inputs[0]
 
@@ -76,20 +76,16 @@ def binary_and(inputs):
 
     return result
         
-def get_doc_urls_from_ids(ids):
+def get_doc_urls_from_ids(ids, docs):
     out = []
     
     if len(ids) == 0:
         return []
-
-    with open("./docs.json", "r", encoding="utf-8") as f:
-        docs = json.load(f)
         
     ids.sort(key=lambda x: x[1], reverse=True)
 
-
     for doc_id, _, in ids[:5]:
-        out.append(docs.get(str(doc_id)))
+        out.append(docs[doc_id])
         
     return out
 
@@ -97,12 +93,35 @@ def get_memory_usage_mb():
     process = psutil.Process(os.getpid())
     return process.memory_info().rss / 1024**2  # in MB
 
+def load_term_map(path):
+    term_map = {}
+    with open(path, "r", encoding="utf-8") as f:
+        for line in f:
+            term, tid = line.strip().split("\t")
+            term_map[term] = int(tid)
+    return term_map
+
+def load_doc_map(path):
+    doc_map = {}
+    with open(path, "r", encoding="utf-8") as f:
+        for line in f:
+            doc_id, url = line.strip().split("\t", 1)
+            doc_map[int(doc_id)] = url
+    return doc_map
+
+def load_toc(path):
+    toc = {}
+    with open(path, "r") as f:
+        for line in f:
+            tid, offset = line.strip().split("\t")
+            toc[int(tid)] = int(offset)
+    return toc
+
 if __name__ == "__main__":
-    with open("mmap.json", "r") as f:
-        toc = json.load(f)
-        
-    with open("./terms.json", "r", encoding="utf-8") as f:
-        terms = json.load(f)
+
+    terms = load_term_map("terms.txt")
+    docs = load_doc_map("docs.txt")
+    toc = load_toc("mmap.txt")
     
     index_file = open("index.txt", "r+b")
     index_mmap = mmap.mmap(index_file.fileno(), 0, access=mmap.ACCESS_READ)
@@ -113,23 +132,40 @@ if __name__ == "__main__":
         search = input("Search term: ")
         start = time.time()
         ids = get_term_id(search, terms)
+        
+        if not ids:
+            print("No documents found containing those terms.\n")
+            continue
+        
         t1 = time.time()
         doc_ids = get_doc_id_from_term_ids(ids, toc, index_mmap)
+        
+        if not any(doc_ids):
+            print("No documents found containing those terms.\n")
+            continue
+
         t2 = time.time()
         intersect = binary_and(doc_ids)
+        
+        if not intersect:
+            print("No documents found containing those terms.")
+            continue
+        
         t3 = time.time()
-        doc_urls = get_doc_urls_from_ids(intersect)
+        doc_urls = get_doc_urls_from_ids(intersect, docs)
+        
         t4 = time.time()
 
         print("\nResults:")
         for url in doc_urls:
             print(url)
 
-        print("Benchmark Timing (in seconds):")
+        print("\nBenchmark Timing (in seconds):")
         print(f"get_term_id:             {t1 - start:.4f}")
         print(f"get_doc_id_from_term_ids:{t2 - t1:.4f}")
         print(f"binary_and:              {t3 - t2:.4f}")
         print(f"get_doc_urls_from_ids:   {t4 - t3:.4f}")
         print(f"Total:                   {t4 - start:.4f}\n")
-        # process = psutil.Process(os.getpid())
-        # print(f"Memory usage: {get_memory_usage_mb():.2f} MB")
+        
+        process = psutil.Process(os.getpid())
+        print(f"Memory usage: {get_memory_usage_mb():.2f} MB")
